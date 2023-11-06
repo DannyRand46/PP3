859ae664aaa2063a60085e8ef012df660974c636
Kevin Hernandez <KJHernandez1@student.fullsail.edu>
2023-11-05 23:21:31 -0500

    challenge room update
    spawn to and from maze
    
    Co-Authored-By: Randon Williams <124099825+DannyRand46@users.noreply.github.com>

--- Assets/Scripts/ChallengeManager.cs
+++ Assets/Scripts/ChallengeManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChallengeManager : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class ChallengeManager : MonoBehaviour
     public GameObject player;
     public GameObject waterObject;
     public GameObject loseBarrier;
+    
     private void Start()
     {
         if(player != null)
@@ -28,13 +30,25 @@ public class ChallengeManager : MonoBehaviour
 
     public void CheckChallengeStatus()
     {
-        if(waterObject.transform.position.y > player.transform.position.y)
+        if (waterObject.transform.position.y > player.transform.position.y)
         {
-            ResetPlayerState();
-            CloseRoom();
+            LoadNewScene();
+
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            LoadNewScene();
+        }
+    }
+
+    private void LoadNewScene()
+    {
+        SceneManager.LoadScene("Prototype2");
+    }
     public void ResetPlayerState()
     {
         player.transform.position = playerStartPos;
