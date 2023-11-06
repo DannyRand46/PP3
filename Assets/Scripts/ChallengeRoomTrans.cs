3f68a3d11b85ca7ee8cb0fd1d56339cd792d2ed8
Kevin Hernandez <KJHernandez1@student.fullsail.edu>
2023-11-05 23:17:38 -0500

    challenge room update
    spawn in and out of challenge room, challenge room hatch added in maze
    
    Co-Authored-By: Randon Williams <124099825+DannyRand46@users.noreply.github.com>

---
+++ Assets/Scripts/ChallengeRoomTrans.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChallengeRoomTrans : MonoBehaviour
+{
+    [SerializeField] GameObject interact;
+
+    bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange && Input.GetButtonDown("Interact"))
+        {
+            for (int i = 0; i < MazeState.instance.items1.Count; i++)
+            {
+                if (MazeState.instance.items1[i].item.CompareTag("WaterRoom"))
+                {
+                    RecursiveDepthFirstSearch.ChangeSpawn(MazeState.instance.items1[i].tile);
+                    break;
+                }
+            }
+
+            SceneManager.LoadScene("Kevin's Scene");
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+        {
+            return;
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            interact.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.isTrigger)
+        {
+            return;
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            interact.SetActive(false);
+        }
+    }
+}
