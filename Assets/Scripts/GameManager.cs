f89ed9eca3acc36bf89d9b9009a80e444442345c
Patrick Hand <114440015+patrickjhand@users.noreply.github.com>
2023-11-05 23:33:31 -0500

    Created in game objects that send player to separate scenes, and back to the main maze.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -43,13 +43,17 @@ public class GameManager : MonoBehaviour
     //Only uncomment code once implemented
     void Awake()
     {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
 
-        instance = this;
-        timeScaleOrig = Time.timeScale;
-        player = GameObject.FindWithTag("Player");
-        playerScript = player.GetComponent<PlayerController>();
-        playerSpawn = GameObject.FindWithTag("Respawn");
-        //enemySpawn = GameObject.FindWithTag("Enemy Spawn");
+            //instance = this;
+            timeScaleOrig = Time.timeScale;
+            player = GameObject.FindWithTag("Player");
+            playerScript = player.GetComponent<PlayerController>();
+            playerSpawn = GameObject.FindWithTag("Respawn");
+            //enemySpawn = GameObject.FindWithTag("Enemy Spawn");
+        
+        
     }
 
 
