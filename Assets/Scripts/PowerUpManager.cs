72ee5f21a9bae880eb103d5cb86f790227ecf56b
Patrick Hand <114440015+patrickjhand@users.noreply.github.com>
2023-11-09 15:00:18 -0500

    PowerUpManager update

--- Assets/Scripts/PowerUpManager.cs
+++ Assets/Scripts/PowerUpManager.cs
@@ -127,7 +127,7 @@ public class PowerUpManager : MonoBehaviour
                 availablePowerUps.Add(type);
             }
 
-            //UpdatePowerUpUI();
+            UpdatePowerUpUI();
         }
     }
 
