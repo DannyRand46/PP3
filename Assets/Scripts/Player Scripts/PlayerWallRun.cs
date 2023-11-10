f62c716d3ffcdb87c64ad0f10dbd471bc94cdd63
Mikeybuzz <114454673+Mikeybuzz@users.noreply.github.com>
2023-11-10 18:45:35 -0500

    setup player wall run and variables

---
+++ Assets/Scripts/Player Scripts/PlayerWallRun.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallRun : MonoBehaviour
+{
+    public LayerMask whatIsWall;
+    public LayerMask whatIsGround;
+    public float wallRunForce;
+    public float maxWallRunTime;
+    private float wallRunTimer;
+
+    private float horizontalDirection;
+    private float verticalDirection;
+
+    public float wallCheckDistance;
+    public float minJumpHeight;
+    private RaycastHit leftWallHit;
+    private RaycastHit rightWallHit;
+    private bool wallLeft;
+    private bool wallRight;
+
+    public Transform orientation;
+    private PlayerController pc;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+}
