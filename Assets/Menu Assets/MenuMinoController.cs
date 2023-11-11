ceaaa020375a1b0b78d8212b0248b46d47c2aab9
Tyler Linton <TSLinton@student.fullsail.edu>
2023-11-10 18:46:11 -0600

    Menu v2
    Fade and loading anim complete

--- Assets/Menu Assets/MenuMinoController.cs
+++ Assets/Menu Assets/MenuMinoController.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuMinoController : MonoBehaviour
 {
     [SerializeField] Animator anim;
     [SerializeField] GameObject startWall;
     [SerializeField] GameObject brokeWall;
-    // Start is called before the first frame update
-public void triggerAnim()
+    [SerializeField] Image fade;
+    [SerializeField] float magnitude;
+
+    bool isFading;
+    float fadeRatio;
+    private void Start()
+    {
+        isFading = false;
+    }
+    public void triggerAnim()
     {
         anim.SetTrigger("PlayButton");
         Transform t = startWall.transform;
         Destroy(startWall);
-        Instantiate(brokeWall, t.position, t.rotation);
+       GameObject wall = Instantiate(brokeWall, t.position, t.rotation);
+       Rigidbody[] wallChildren = wall.GetComponentsInChildren<Rigidbody>();
+        for (int i = 0; i < wallChildren.Length; i++)
+        {
+            wallChildren[i].AddForce(transform.forward*magnitude, ForceMode.Impulse);
+        }
+    }
+    public void LoadGame()
+    {
+        isFading = true;
+        fadeRatio = 0;
+    }
+
+    private void Update()
+    {
+       if (isFading)
+        {
+            fadeRatio += Time.deltaTime;
+            fade.color = new Color32(1, 2, 3, (byte)(fadeRatio * 255));
+            if(fadeRatio >= 1)
+            {
+                //TODO: Check for correct Scene Load
+                SceneManager.LoadScene("ProtoType2");
+            }
+        }
     }
 }
